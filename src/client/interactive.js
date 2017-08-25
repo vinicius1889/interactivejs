68371829ccb37c2919792d7aacef35545a817a76
vinicius1889 <vinicius1889@gmail.com>
2017-08-25 18:53:53 -0300

    feat: adicionando a feature das rooms

--- src/client/interactive.js
+++ src/client/interactive.js
@@ -14,6 +14,7 @@ socket.on('doRegistration',function(){
 });
 
 socket.on('online-users',function(data){
+    console.log(data)
     document.getElementById("onlineusers").innerText=data.length
 });
 
